945dca3191ff4236ba0523a5cb8eb05430ce5608
Jeffrey Prouty <jcprouty@dhcp-18-30-89-154.dyn.mit.edu>
2018-11-19 19:45:06 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -8,19 +8,28 @@ import DataEntry from './DataEntry';
 class App extends Component {
   render() {
     return (
-      <div className='outer-div'>
-        <h2>Progress on Today's Tasks</h2>
-        <p>The percentage of allotted time for today's tasks completed thus far,
-        compared to a target percentage for this time of day.</p>
-        <ProgressDisplay />
-        <h2>Recent Productivity Scores</h2>
-        <p>The percentage of allotted time completed each day in the past week
-        compared to the goal, along with time spent on optional tasks.</p>
-        <TaskDisplay />
-        <h2>Data For Today</h2>
-        <p>A quick way to enter information one keeps track of each day, such as
-        hours of sleep.</p>
-        <DataEntry />
+      <div className='container'>
+        <h1>Dashboard</h1>
+        <div className='outer-div'>
+          <div className='description'>
+            <h2>Progress on Today's Tasks</h2>
+            <p>The percentage of allotted time for today's tasks completed thus far,
+            compared to a target percentage for this time of day.</p>
+          </div>
+          <ProgressDisplay />
+          <div className='description'>
+            <h2>Recent Productivity Scores</h2>
+            <p>The percentage of allotted time completed each day in the past week
+            compared to the goal, along with time spent on optional tasks.</p>
+          </div>
+          <TaskDisplay />
+          <div className='description'>
+            <h2>Data For Today</h2>
+            <p>A quick way to enter information one keeps track of each day, such as
+            hours of sleep.</p>
+          </div>
+          <DataEntry />
+        </div>
       </div>
     );
   }
